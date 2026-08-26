378946917053ca85f8e7ed8e9c12358417bd7e5e
mustafahassan065 <mustafaprogrammer786@gmail.com>
2026-08-26 00:59:07 -0700

    fix the generate score image

--- generate_score_image.py
+++ generate_score_image.py
@@ -99,7 +99,7 @@ def upload_image_to_s3(image_bytes: bytes, filename: str = None) -> str:
         's3',
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        region_name=os.getenv("AWS_REGION", "eu-north-1")
+        region_name='eu-north-1'
     )
     bucket = os.getenv("AWS_BUCKET_NAME")
     if not filename:
